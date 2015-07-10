301c82056a009382d6d28715ba66d9e7fa4f0963
Brian Oxley <boxley@thoughtworks.com>
2015-07-10 13:49:36 -0500

    Streamier code.

--- health/src/main/java/hm/binkley/scratch/health/HealthModule.java
+++ health/src/main/java/hm/binkley/scratch/health/HealthModule.java
@@ -9,6 +9,8 @@ import org.kohsuke.MetaInfServices;
 import javax.inject.Singleton;
 import java.util.ServiceLoader;
 
+import static java.util.stream.StreamSupport.stream;
+
 /**
  * {@code HealthModule} wires {@link HealthVerticle}.
  *
@@ -33,11 +35,12 @@ public class HealthModule
     @Singleton
     public static HealthVerticle provideHealthVerticle(
             final ServiceLoader<Verticle> verticles) {
-        for (final Verticle v : verticles)
-            if (v instanceof HealthVerticle)
-                return (HealthVerticle) v;
-        throw new IllegalStateException(
-                "No HealthVerticle in META-INF/services");
+        return stream(verticles.spliterator(), true).
+                filter(v -> v instanceof HealthVerticle).
+                findFirst().
+                map(HealthVerticle.class::cast).
+                orElseThrow(() -> new IllegalStateException(
+                        "No HealthVerticle in META-INF/services"));
     }
 
     @Override
